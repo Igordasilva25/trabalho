0bd2d477e077cfac3553e89c0159772624f4f68e
Igor da Silva <igordasilva25270@gmail.com>
2018-04-22 07:44:41 -0300

    Natureza em movimento - Inicio do site

---
+++ Login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Natureza em movimento!!</title>
+        <link href="css/css.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <header>
+            <h1><a href="index.php">Natureza em movimento!</a></h1>
+            <nav>
+                <a href="Login.php">Login</a> |
+                <a href="Cadastro.php">Cadastro</a>
+            </nav>
+        </header>
+        <section id="Banner">
+            
+        </section>
+        <main>
+            <section class="fundo" style="height: 300px;">
+                <h1>Login</h1>
+            <section class="eixo">
+                <form method="POST">
+                    <label>Email</label>
+                    <input name="EmaileForm" type="email" required="" placeholder="Digite seu email aqui...">
+                    <label>Senha</label>
+                    <input name="SenhaForm" type="password" required="" placeholder="Digite sua senha aqui...">
+                    <button type="submit" name="Enviar">Logar Usuário</button>
+                </form>
+            </section>
+            </section>
+        </main>
+        <footer>
+            Website criado e desenvolvido por Saymon, Barbara, Igor e Vitoria.
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Natureza em movimento!!</title>
+        <link href="css/css.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <header>
+            <h1><a href="index.php">Natureza em movimento!</a></h1>
+            <nav>
+                <a href="Login.php">Login</a> |
+                <a href="Cadastro.php">Cadastro</a>
+            </nav>
+        </header>
+        <section id="Banner">
+            
+        </section>
+        <main>
+            <section class="fundo">
+                <h1>Publique fotos do nosso lindo planeta</h1>
+            <section class="eixo">
+                <figure>
+                    <img src="Imagens/12752_trilha-na-floresta.jpg" alt=""/>
+                    <figcaption></figcaption>
+                </figure>
+                <figure>
+                    <img src="Imagens/420804_doroga_les_zabor_pejzazh_2560x1440_www.Gde-Fon.com.jpg" alt=""/>
+                    <figcaption></figcaption>
+                </figure>
+                <figure>
+                    <img src="Imagens/be876293a1f8946d4a3880e5cb61a54e.jpg" alt=""/>
+                    <figcaption></figcaption>
+                </figure>
+                <figure>
+                    <img src="Imagens/dark-forest-wallpaper-HD3.jpg" alt=""/>
+                    <figcaption></figcaption>
+                </figure>
+                <figure>
+                    <img src="Imagens/floresta-52058a17e4a63.jpg" alt=""/>
+                    <figcaption></figcaption>
+                </figure>
+                <figure>
+                    <img src="Imagens/images.jpg" alt=""/>
+                    <figcaption></figcaption>
+                </figure>
+                <figure>
+                    <img src="Imagens/wallpaper-artistico-da-floresta.jpg" alt=""/>
+                    <figcaption></figcaption>
+                </figure>
+                <figure>
+                    <img src="Imagens/wallpaper-floresta-coberta-neve.jpg" alt=""/>
+                    <figcaption></figcaption>
+                </figure>
+                <figure>
+                    <img src="Imagens/wallpaper-quente-floresta_1.jpg" alt=""/>
+                    <figcaption></figcaption>
+                </figure>
+            </section>
+            </section>
+        </main>
+        <footer>
+            Website criado e desenvolvido por Saymon, Barbara, Igor e Vitoria.
+        </footer>
+    </body>
+</html>
